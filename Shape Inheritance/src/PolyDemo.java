fd61ea809b66b4f6ddfb17da08dee50539ee7108
veersaini <veersaini@live.com>
2023-11-02 14:55:36 -0700

    Shape Inheritance pt 2

--- Shape Inheritance/src/PolyDemo.java
+++ Shape Inheritance/src/PolyDemo.java
@@ -2,14 +2,16 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 
-/*
+/**
  * Class PolyDemo (is a JFrame) and PolyDemoPanel (is a JPanel)
- * 
+ *
  * Author: Rob Nash
  */
 class PolyDemo extends JFrame {
-	
-	
+
+	/**
+	 * Constructor for the PolyDemo class.
+	 */
 	public PolyDemo() {
 		getContentPane().add( new PolyDemoPanel() );
 		//just some windowing stuff that must happen for all Frames
@@ -17,95 +19,78 @@ class PolyDemo extends JFrame {
 		setVisible( true );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	}
-	
-	
+
+	/**
+	 * The main method to execute the PolyDemo application.
+	 * @param args The command line arguments
+	 */
 	public static void main( String args[] ) {
 		PolyDemo myApp = new PolyDemo();
-
 	}
-	
-	
-	//this is our first "inner" or internal class 
-	//the purpose of this class is solely to support the JFrame class above, and I don't want it reused in arbitrary contexts, so by nesting this class here
-	//I can indicate the intent a bit more clearly that this class "goes with" the class above it
-	//In general, each class is a separate entity that should be contained in a separate file
-	public class PolyDemoPanel extends JPanel {		
-		Shape[] myShapes= new Shape[20];
-		
-		public PolyDemoPanel() {
-			//Shape a = new Shape( getRandInt(), getRandInt());
-			//Shape b = new Circle( getRandInt(), getRandInt(), getRandInt() );
-		
-			//a = new Square(getRandInt(), getRandInt(), getRandInt(), getRandInt() );
-		
-			//a = getRandShape();
 
-			//( (Circle) b ).getRadius();
-		
-		
-			/*********************************************************************************************************************
-			* Code for populating our myShapes changes minimally when new classes are introduced (only in getRandShape())
-			*********************************************************************************************************************/
+	/**
+	 * The inner class PolyDemoPanel which extends JPanel.
+	 * Purpose of this class is to support the JFrame class.
+	 */
+	public class PolyDemoPanel extends JPanel {
+		Shape[] myShapes= new Shape[20];
+
+		/**
+		 * Constructor for PolyDemoPanel. Populates myShapes array.
+		 */
+		public PolyDemoPanel() {
+			//Code for populating our myShapes changes minimally when new classes are introduced (only in getRandShape())
 			for( int i = 0; i < 20; i++ ) {
 				myShapes[i] =  getRandShape();
 			}
 		}
-		
-		/*********************************************************************************************************************
-		 * Code for drawing our shapes doesn't change at all! Since we intended to take advantage of polymorphism, we coded 
-		 * this "in general" with respect to the superclass, and not specific to any subclass.
-		 *********************************************************************************************************************/
+
+		/**
+		 * Paint method to draw shapes on the panel.
+		 * @param g The Graphics object
+		 */
 		public void paint( Graphics g ) {
-			super.paint(g);  //don't remove - required for GUI widgets to draw correctly
-			/************************
-			 * Late Binding Demo
-			 ************************/
+			super.paint(g);  //required for GUI widgets to draw correctly
+
+			//Late Binding Demo
 			for(int i = 0; i < myShapes.length; i++ ){
 				//which draw method is invoked here? There are many forms of the method (polymorphic), so which is chosen?
 				//Java has RTTI about every object, and it uses this info to choose the correct method to invoke!
 				myShapes[i].draw(g);
 			}
 		}
-			
-		
+
+		/**
+		 * Generate a random integer.
+		 * @return A random integer value
+		 */
 		public int getRandInt() {
-			return ( (int) ( Math.random() * 200 ) );	
+			return ( (int) ( Math.random() * 200 ) );
 		}
-		
+
+		/**
+		 * Get a random shape.
+		 * @return A Shape object
+		 */
 		public Shape getRandShape() {
 			Shape retVal = null;
 			final int x = getRandInt();
 			final int y = getRandInt();
-			
-			
-			/********************************
-			 * Polymorphic extensibility demo
-			 *
-			 *******************************/
+
+			//Polymorphic extensibility demo
 			switch( (int)(Math.random() * 4) ) {
-				case 0: 	retVal = new Circle(getRandInt(), getRandInt(), getRandInt());
-							break;
-				case 1: 	retVal = new Rectangle(10, 50, 25, 15);
-							break;
-				case 2: 	retVal = new Kite(getRandInt(), getRandInt(), getRandInt(), getRandInt(), Color.pink);
-							break;
-				case 3: 	retVal = new IsoscelesTriangle(getRandInt(), getRandInt(), getRandInt(), getRandInt(), Color.green);
-							break;				
+				case 0:     retVal = new Circle(getRandInt(), getRandInt(), getRandInt());
+					break;
+				case 1:     retVal = new Rectangle(10, 50, 25, 15);
+					break;
+				case 2:     retVal = new Kite(getRandInt(), getRandInt(), getRandInt(), getRandInt(), Color.pink);
+					break;
+				case 3:     retVal = new IsoscelesTriangle(getRandInt(), getRandInt(), getRandInt(), getRandInt(), Color.green);
+					break;
 			}
-		
+
 			return retVal;
 		}
-	}	
-	
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
